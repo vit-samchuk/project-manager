9f7e85d83a67a54b2236982c077dec9e84650c1b
vit.samchuk <vit.samchuk@gmail.com>
2025-08-10 10:21:33 +0300

    update

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 function authMiddleware(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1];
   if (token !== process.env.API_TOKEN) {
@@ -5,5 +7,20 @@ function authMiddleware(req, res, next) {
   }
   next();
 }
+function authMiddlewareGitHub(req, res, next) {
+  const secret = process.env.API_TOKEN;
+  const signature = req.headers['x-hub-signature-256'];
+  if (!signature) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  const hmac = crypto.createHmac('sha256', secret);
+  const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  const verified = crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest));
+  
+  if (!verified) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+}
 
-module.exports = authMiddleware;
+module.exports = authMiddlewareGitHub;
--- routes/projects.route.js
+++ routes/projects.route.js
@@ -4,40 +4,30 @@ const { addProject, removeProject } = require('../services/projects.service');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  console.log(req)
-  res.send('ok')
-})
-router.post('/gh-hook', express.json({ type: '*/*' }), async (req, res) => {
+router.post('/gh-hook', auth, async (req, res) => {
   console.log('Headers:', req.headers);
   console.log('Body:', req.body);
   res.send({ success: true });
-  return
-  const { name, url, description } = req.body;
-  if (!name || !url) return res.status(400).send({ error: 'Missing name or url' });
-
-  await addProject({ name, url, description });
-  res.send({ success: true });
 });
 
-router.post('/', auth, async (req, res) => {
-  console.log(req.body)
-  return
-  const { name, url, description } = req.body;
-  if (!name || !url) return res.status(400).send({ error: 'Missing name or url' });
+// router.post('/', auth, async (req, res) => {
+//   console.log(req.body)
+//   return
+//   const { name, url, description } = req.body;
+//   if (!name || !url) return res.status(400).send({ error: 'Missing name or url' });
 
-  await addProject({ name, url, description });
-  res.send({ success: true });
-});
+//   await addProject({ name, url, description });
+//   res.send({ success: true });
+// });
 
-router.delete('/:branch', auth, async (req, res) => {
-  const { name } = req.params.branch;
-  console.log(name)
-  return;
-  if (!name) return res.status(400).send({ error: 'Missing name' });
+// router.delete('/:branch', auth, async (req, res) => {
+//   const { name } = req.params.branch;
+//   console.log(name)
+//   return;
+//   if (!name) return res.status(400).send({ error: 'Missing name' });
 
-  await removeProject(name);
-  res.send({ success: true });
-});
+//   await removeProject(name);
+//   res.send({ success: true });
+// });
 
 module.exports = router;
